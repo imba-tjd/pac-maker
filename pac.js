b6824d5c0b559975ac82ed332328aad43e29992d
imba-tjd <109224573@qq.com>
2021-10-03 13:31:49 +0800

    去掉不必要的分号

--- pac.js
+++ pac.js
@@ -1,29 +1,29 @@
-al = JSON.parse('allowlist');
-bl = JSON.parse('blocklist');
+al = JSON.parse('allowlist')
+bl = JSON.parse('blocklist')
 
-proxy = '__PROXY__';
-direct = 'DIRECT;';
+proxy = '__PROXY__'
+direct = 'DIRECT;'
 if (proxy == '__PRO' + 'XY__') // 压缩时会被优化掉，手动替换一下
-    proxy = eval('__PRO' + 'XY__');
+    proxy = eval('__PRO' + 'XY__')
 
 hop = Object.hasOwnProperty;
 
 function FindProxyForURL(_, host) {
     if (hop.call(al, host))
-        return direct;
+        return direct
 
-    var suffix;
-    var pos = host.lastIndexOf('.');
+    var suffix
+    var pos = host.lastIndexOf('.')
     while (true) {
         if (pos <= 0) {
             if (hop.call(bl, host))
-                return proxy;
+                return proxy
             else
-                return direct;
+                return direct
         }
         suffix = host.slice(pos + 1);
         if (hop.call(bl, suffix))
-            return proxy;
-        pos = host.lastIndexOf('.', pos - 1);
+            return proxy
+        pos = host.lastIndexOf('.', pos - 1)
     }
-};
+}
